0b07308d9561e6600de7e95781f9ab8839b30441
alanviegas <32642756+alanviegas@users.noreply.github.com>
2023-04-30 10:32:33 -0300

    Update pipeline.py

--- src/pyspark/neuralmed_raw/pipeline.py
+++ src/pyspark/neuralmed_raw/pipeline.py
@@ -10,9 +10,12 @@ from neuralmed_raw.utils.helpers import parse_json
 
 def main(table_name: str, spark: Any):
     """
-    todo
+    Função principal que executa pipeline - leitura dos arquivos de dados, faz o parse dos dados 
+    (converte datatypes) de acordo com os parâmetros do arquivo de configuração,e 
+    carrrega no Storage como arquivo parquet particionado
     """
 
+    # arquivo de configuração com os schemas das tabelas
     with open(CONFIG_SCHEMAS_FILE, "r") as j:  # type: ignore
         config_schemas = json.loads(j.read())
 
@@ -23,8 +26,10 @@ def main(table_name: str, spark: Any):
         target_columns.append(v)
 
     logging.info("read json files")
+    # carrega os json para para um dataframe spark
     rawJSON = spark.read.json(config_schemas[table_name]["path_files"], multiLine="true")
-
+    
+    # se a tabela for "Label" a leitura dos jsons é um pouco diferente, por que os dados estão aninhados
     if table_name == "Label":
         rawDF = rawJSON.withColumn("labels", F.explode("content.labels"))
         rawDF = rawDF.select("content.id", "labels.*")
@@ -35,15 +40,18 @@ def main(table_name: str, spark: Any):
     parsedRDD = rawDF.rdd.map(lambda x: parse_json(x, target_columns))
 
     columns_list = list(dict(target_columns).keys())
-
+    
+    # Converte os dados tratados para um outro dataframe 
     parsedDF = parsedRDD.toDF(columns_list)
 
+    # inclui um data de carga
     parsedFinalDF = parsedDF.withColumn("dt_load", F.lit(DATE_LOAD))
 
     parsedFinalDF.printSchema()
     parsedFinalDF.show()
 
     logging.info("saving on bucket raw")
+    # salva no storage particionado pelo data de carga em formato parquet e compressão snappy 
     parsedFinalDF.write.partitionBy("dt_load").mode("overwrite").format("parquet").option(
         "compression", "snappy"
     ).save(f"{RAW_FILES_PATH}/{table_name}")
